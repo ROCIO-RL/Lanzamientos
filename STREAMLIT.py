2b82a8e358ae2072ffb25dc2e72cf761c12564ca
ROCIO-RL <186742156+ROCIO-RL@users.noreply.github.com>
2025-07-23 22:03:27 +0000

    Grafico

--- STREAMLIT.py
+++ STREAMLIT.py
@@ -250,7 +250,7 @@ if st.session_state.graficar:
                     y='independent'
                 ).properties(
                     title=f"Predicción vs Sellout - {prod_sel}",
-                    width=1000,
+                    width=1200,
                     height=400
                 )
 
