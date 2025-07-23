18aeecc9ef3bba828c83181dc24aa088ad7e59db
ROCIO-RL <186742156+ROCIO-RL@users.noreply.github.com>
2025-07-23 21:57:56 +0000

    Grafico

--- STREAMLIT.py
+++ STREAMLIT.py
@@ -232,7 +232,7 @@ if st.session_state.graficar:
                 # Línea punteada para temperatura (sin 'name')
                 line = alt.Chart(df_melt).mark_line(
                     strokeDash=[4, 4],
-                    color='black'
+                    color='purple'
                 ).encode(
                     x=alt.X('SemNumero:O'),
                     y=alt.Y('TEMPERATURA:Q',
